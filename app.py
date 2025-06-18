a866af4ac853f238446ce62406449923e3632f69
YellenkiAbhilash <abhilashyellenki@gmail.com>
2025-06-18 15:05:54 +0530

    Updated app.py to disable debug mode for production

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def download():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render uses PORT=10000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)  # Disable debug mode for production
